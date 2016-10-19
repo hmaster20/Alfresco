28280f8147548fdfadcdb745b6b347b69d6c89cb
Sergei <hmaster20@gmail.com>
2016-10-19 12:00:04 +0300

    Исправлена проблема с кодировкой в Ubuntu

--- updateArg.sh
+++ updateArg.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 # Update of Alfresco
 # sudo chmod +x updateArg.sh
-# sudo sh ./updateArg.sh 
+# sudo sh ./updateArg.sh
+
 
 echo "#####################" 
 echo "#  Update Alfresco  #" 
 echo "#####################" 
 
 # Configuration:
-  FOLDER="/opt/alfresco-5.0.d"				# Расположение Alfresco
-  FOLDER_update="/home/kravetsma/distr/"	# Расположение билда
-  FOLDER_current=$PWD						# Текущий рабочий каталог
-  result_ok="BUILD SUCCESSFUL"				# Результат успешного обновления
-  Time=0									# Текущее время, сек
-  TimeWait=5								# Интервалы проверки, сек
-  TimeWaitmax=240							# Максимальное время ожидания, сек
-  PathBuild=""								# Переменная для хранения пути к сборке
+  FOLDER="/opt/alfresco-5.0.d"                          # Расположение Alfresco
+  FOLDER_update="/home/kravetsma/distr/"        # Расположение билда
+  FOLDER_current=$PWD                                           # Текущий рабочий каталог
+  result_ok="BUILD SUCCESSFUL"                          # Результат успешного обновления
+  Time=0                                                                        # Текущее время, сек
+  TimeWait=5                                                            # Интервалы проверки, сек
+  TimeWaitmax=240                                                       # Максимальное время ожидания, сек
+  PathBuild=""                                                          # Переменная для хранения пути к сборке
   
 
 # Проверка наличия параметра запуска
 if [ -z "$1" ]
   then
-	  echo "Do not Set option. Run the script is not possible!"
-	  exit 1
+          echo "Do not Set option. Run the script is not possible!"
+          exit 1
   else
-	  # Проверка существования папки
-	  if [ -d "$FOLDER_update$1" ]; then
-	  	  PathBuild=$FOLDER_update$1
- 		  echo "For the installation will be used catalog \"$PathBuild\""
-	  else
-	      echo "Directory \"$1\" in $FOLDER_update does not exist!"
-		  exit 1
-	  fi
+          # Проверка существования папки
+          if [ -d "$FOLDER_update$1" ]; then
+                  PathBuild=$FOLDER_update$1
+                  echo "For the installation will be used catalog \"$PathBuild\""
+          else
+              echo "Directory \"$1\" in $FOLDER_update does not exist!"
+                  exit 1
+          fi
 fi
 
 # Фактический запуск скрипта
@@ -59,19 +60,19 @@ al_start()
 # Функция - обновления Alfresco из полученной сборки
 al_update()
 {
-if [ -d "$PathBuild" ]; then						# Проверка существования папки
-   if [ -f "$PathBuild/install-amp.xml" ]; then		# Проверка существования инсталятора    
-	  	  cd $PathBuild
+if [ -d "$PathBuild" ]; then                                            # Проверка существования папки
+   if [ -f "$PathBuild/install-amp.xml" ]; then         # Проверка существования инсталятора    
+                  cd $PathBuild
           sudo ant -Dalfresco.install=$FOLDER -f install-amp.xml > $FOLDER_current/update.log
-		  if cat $FOLDER_current/update.log | grep -q "$result_ok"	# Проверка корректности обновления
+                  if cat $FOLDER_current/update.log | grep -q "$result_ok"      # Проверка корректности обновления
              then
-	             echo "Build is installed successfully!"
-	      else
+                     echo "Build is installed successfully!"
+              else
                  echo "Error!!. See the event log: $FOLDER_current/update.log"
-				 exit 1
+                                 exit 1
           fi
-	  else
-	  	  echo "File \"install-amp.xml\" in \"$PathBuild\" does not exist!"		 
+          else
+                  echo "File \"install-amp.xml\" in \"$PathBuild\" does not exist!"              
    fi
 else
    echo "Не верно указан каталог сборки!"
@@ -82,7 +83,7 @@ fi
 # Функция - Таймер
 timer()
 {
-	Time=$(($Time+$1)) # сумма значения + аргумент (входное значение)
+        Time=$(($Time+$1)) # сумма значения + аргумент (входное значение)
     if [ "$Time" -ge "$TimeWaitmax" ]; then echo "Files are not updated. Timeout!"; exit 1;fi
 }
 
@@ -101,8 +102,8 @@ done
 if [ -d "$folder1" ] || [ -d "$folder2" ]; then
   cd $FOLDER_update
   sudo cp yui-common* $folder1
-  sudo cp MessagesWebScript.class $folder2	
-  echo "Files updated. Click the link /share/page/index and and press \"Refresh Web Scripts\" !!!"		 
+  sudo cp MessagesWebScript.class $folder2      
+  echo "Files updated. Click the link /share/page/index and and press \"Refresh Web Scripts\" !!!"               
 fi
 }
 
@@ -156,8 +157,19 @@ echo "...................................................."
 # 5 - Files update
 #------------------------------------------
 
-  al_file_update		 # Обновление файлов возможно только после полного запуска сервера
+  al_file_update                 # Обновление файлов возможно только после полного запуска сервера
   echo "...................................................." 
   echo "$(date +%d.%m.%Y) ($(date +%H.%M:%S)) # The script is completed." 
   echo "...................................................."   
   
+
+
+
+
+
+
+
+
+
+
+
